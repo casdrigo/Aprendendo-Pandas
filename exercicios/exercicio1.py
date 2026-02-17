39afac59bfc066f3254091bcab0dbebdab47ffae
rodrigo <rodrigocassolp@gmail.com>
2026-02-17 14:22:02 -0300

    retirando leituras nulas e duplicadas

--- exercicios/exercicio1.py
+++ exercicios/exercicio1.py
@@ -12,10 +12,20 @@ df.to_csv('../data/exercicio/dump_barragem_telix1.csv') #salvando 1 modificaçã
 
 # %%
 
-df[df.duplicated()]
+df[df.duplicated()]     #mostra somente as duplicatas
 # %%
 df_sem_duplicatas = df.drop_duplicates()
 df_sem_duplicatas
 #%%
 df_sem_duplicatas.to_csv('../data/exercicio/dump_barragem_telix2-semDuplicatas.csv')
 # %%
+df_sem_duplicatas = df_sem_duplicatas[df_sem_duplicatas.status != 'OFFLINE']
+#acima estou dizendo que quero que a dfSemDuplicatas seja igual a mesma porém com a coluna
+#status sendo diferente de offline
+
+df_sem_duplicatas
+# %%
+df_limpo = df_sem_duplicatas.dropna(subset=['leitura_mm'])
+df_limpo
+# %%
+df_limpo.to_csv('../data/exercicio/dump_barragem_telix3-limpoSemLeiturasNulas.csv')
