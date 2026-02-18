52e7047df1e2dfa06dd5caa5dba0cd1f454ebf87
rodrigo <rodrigocassolp@gmail.com>
2026-02-18 11:40:53 -0300

    entendendo filtros em pandas

---
+++ dia03/01_filtro.py
@@ -0,0 +1,33 @@
+#%%
+import pandas as pd
+
+df = pd.read_csv("../data/transacoes.csv", sep =";")
+df.head()
+
+#%%
+pontos = [10 ,1 ,1 ,1 ,50 ,100 ,130 ,1 ,1 ,30 ,25 ,50]
+filtro =[]
+
+valores_50_mais = []
+for i in pontos:
+    filtro.append(i>=50)
+resultado =[]
+for i in range(len(pontos)):
+    if filtro[i]:
+        resultado.append(pontos[i])
+
+resultado
+
+#%%
+
+brinquedo = pd.DataFrame(
+    {
+        "nome"  :   ["teo", "nah","mah"],
+        "idade" :   [32,35,14],
+        "uf"    :   ["sp","pr","rj"],
+    }
+)
+
+filtro = brinquedo["idade"] >= 18
+
+brinquedo[filtro]
